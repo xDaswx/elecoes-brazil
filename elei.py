e8b7749fb0d1f9adc0139629e953b06e6d0753a8
Dasw <69166890+xDaswx@users.noreply.github.com>
2022-10-05 22:54:31 -0300

    fix link

--- elei.py
+++ elei.py
@@ -11,10 +11,11 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
 }
 
-r = requests.get('https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json', headers=headers).json()
+
 
 
 def check():
+		r = requests.get('https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json', headers=headers).json()
 		print("Horario de atualização:", r['hg'])
 		print("Apuração:", r['pst'])
 		print("------------------")
