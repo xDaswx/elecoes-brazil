0af2c718817b78e2dc89375edaad039be734cef7
Dasw <69166890+xDaswx@users.noreply.github.com>
2022-10-30 01:38:39 -0300

    min

--- elei.py
+++ elei.py
@@ -1,17 +1,5 @@
 import requests,json,time
-headers = {
-    'authority': 'resultados.tse.jus.br',
-    'accept': 'application/json, text/plain, */*',
-    'accept-language': 'en-US,en;q=0.5',
-    'referer': 'https://resultados.tse.jus.br/oficial/app/index.html',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'sec-gpc': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
-}
-
-
+headers = {'authority': 'resultados.tse.jus.br','accept': 'application/json, text/plain, */*','accept-language': 'en-US,en;q=0.5','user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',}
 
 
 def check():
